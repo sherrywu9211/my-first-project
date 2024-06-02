a79b1b661e512787dcb6ffbf92d1bfda37ef4eba
Sherry <l223971013@gmail.com>
2024-06-02 13:53:27 +0800

    ok

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet("/hello2")
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
